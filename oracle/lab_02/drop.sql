d02ed3ffc2817cb5c275e89c92acc3503d68750f
Антон <akrik@192.168.1.6>
2021-10-31 23:22:25 +0300

    a

--- oracle/lab_02/drop.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE Balance;
-DROP TABLE CompanyOwner;
-DROP TABLE Company;
-DROP TABLE StockExchange;
-DROP TABLE StockMarket;
--- oracle/lab_02/tess.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM CompanyOwner
-ORDER BY balance_id;
